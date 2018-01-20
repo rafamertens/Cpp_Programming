1c5ff22e8a328894e8adf1c4e2ce0b0f05b6cc11
rafamertens <mertens.rafa@gmail.com>
2018-01-20 23:13:28 +0100

    main.cpp
    Hello World in C++ with a timer, that may be useful later on...

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+#include <conio.h>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+	clock_t tStart = clock();  // start timer for main function
+
+	cout << "Hello";
+	cout << " World!\n\n";
+
+	// Print the execution time and wait for a char to exit the program
+	printf("Time taken: %.3fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
+
+	_getche();
+	return 0;
+}
